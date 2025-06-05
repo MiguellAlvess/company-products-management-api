1baefad0ccfe24976b22ff25f9251c9253a0f2a4
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-05 19:21:54 -0300

    feat: add EmailAlreadyInUseError

--- src/errors/company.js
+++ src/errors/company.js
@@ -1,6 +1,13 @@
-export class CnpjAlreadyExistsError extends Error {
+export class CnpjAlreadyInUseError extends Error {
     constructor(cnpj) {
         super(`The provided ${cnpj} is already in use`)
         this.name = 'CnpjAlreadyExistsError'
+    }
+}
+
+export class EmailAlreadyInUseError extends Error {
+    constructor(email) {
+        super(`The provided ${email} is already in use`)
+        this.name = 'EmailAlreadyExistsError'
     }
 }
