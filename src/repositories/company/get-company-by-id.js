ce5885b4d6340746ca473dd06a950210319e6c03
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-05 19:15:48 -0300

    feat: add PostgresGetCompanyByIdRepository

---
+++ src/repositories/company/get-company-by-id.js
@@ -0,0 +1,11 @@
+import { prisma } from '../../../prisma/prisma.js'
+
+export class PostgresGetCompanyByIdRepository {
+    async execute(companyId) {
+        return await prisma.company.findUnique({
+            where: {
+                companyId,
+            },
+        })
+    }
+}
