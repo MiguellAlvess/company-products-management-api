cd82a20236ee72b4e7dfd546174701d107a08934
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-05 20:23:42 -0300

    refactor: rename PostgresGetCompanyByEmailRepository

--- src/repositories/company/get-company-by-email.js
+++ src/repositories/company/get-company-by-email.js
@@ -1,6 +1,6 @@
 import { prisma } from '../../../prisma/prisma.js'
 
-export class PostgresGetRestaurantByEmailRepository {
+export class PostgresGetCompanyByEmailRepository {
     async execute(email) {
         return await prisma.restaurant.findUnique({
             where: {
