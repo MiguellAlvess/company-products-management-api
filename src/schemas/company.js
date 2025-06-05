af2f09c56c190821e9eab746f65510ebe0d571ff
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-05 19:44:50 -0300

    feat: add createCompanySchema

---
+++ src/schemas/company.js
@@ -0,0 +1,41 @@
+import zod, { z } from 'zod'
+import { validateCNPJ } from '../schemas/utils.js'
+
+export const createCompanySchema = zod.object({
+    name: z
+        .string({
+            required_error: 'Name is required',
+        })
+        .trim()
+        .min(1, {
+            message: 'Name is required',
+        }),
+
+    cnpj: z
+        .string({ required_error: 'CNPJ is required' })
+        .trim()
+        .refine((cnpj) => validateCNPJ(cnpj), {
+            message: 'CNPJ is invalid',
+        }),
+
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .trim()
+        .email({
+            message: 'Please provide a valid email address',
+        })
+        .trim()
+        .min(1, {
+            message: 'Email is required',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must be at least 6 characters',
+        }),
+})
