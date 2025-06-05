eeddf82ab4f7dbe212035d0e625806503d9acc5f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-05 20:36:59 -0300

    fix: correct prisma model name in PostgresGetCompanyByCnpjRepository

--- src/repositories/company/get-company-by-cnpj.js
+++ src/repositories/company/get-company-by-cnpj.js
@@ -2,7 +2,7 @@ import { prisma } from '../../../prisma/prisma.js'
 
 export class PostgresGetCompanyByCnpjRepository {
     async execute(cnpj) {
-        return await prisma.restaurant.findUnique({
+        return await prisma.company.findUnique({
             where: {
                 cnpj,
             },
