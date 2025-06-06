73be7c416d2d72594f686e2cebb00086d8071f72
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-06 09:03:54 -0300

    feat: add PATCH /companies route

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -2,9 +2,18 @@ import { Router } from 'express'
 import {
     makeCreateCompanyController,
     makeGetCompanyByIdController,
+    makeUpdateCompanyController,
 } from '../factories/controllers/company.js'
 
 export const companiesRouter = Router()
+
+companiesRouter.post('/', async (req, res) => {
+    const createCompanyController = makeCreateCompanyController()
+
+    const { body, statusCode } = await createCompanyController.execute(req)
+
+    res.status(statusCode).json(body)
+})
 
 companiesRouter.get('/:companyId', async (req, res) => {
     const getCompanyByIdController = makeGetCompanyByIdController()
@@ -13,10 +22,11 @@ companiesRouter.get('/:companyId', async (req, res) => {
 
     res.status(statusCode).json(body)
 })
-companiesRouter.post('/', async (req, res) => {
-    const createCompanyController = makeCreateCompanyController()
 
-    const { body, statusCode } = await createCompanyController.execute(req)
+companiesRouter.patch('./:companyId', async (req, res) => {
+    const updateCompanyController = makeUpdateCompanyController()
+
+    const { body, statusCode } = await updateCompanyController.execute(req)
 
     res.status(statusCode).json(body)
 })
