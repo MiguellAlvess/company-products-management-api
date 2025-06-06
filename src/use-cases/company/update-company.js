4c0d4906128bbd851ecea28327c08500350f82d2
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-06 08:54:17 -0300

    feat: add UpdateCompanyUseCase

---
+++ src/use-cases/company/update-company.js
@@ -0,0 +1,64 @@
+import {
+    CnpjAlreadyInUseError,
+    EmailAlreadyInUseError,
+} from '../../errors/index.js'
+
+export class UpdateCompanyUseCase {
+    constructor(
+        updateCompanyRepository,
+        getCompanyByCnpjRepository,
+        getCompanyByEmailRepository,
+        passwordHasherAdapter,
+    ) {
+        this.updateCompanyRepository = updateCompanyRepository
+        this.getCompanyByCnpjRepository = getCompanyByCnpjRepository
+        this.getCompanyByEmailRepository = getCompanyByEmailRepository
+        this.passwordHasherAdapter = passwordHasherAdapter
+    }
+    async execute(companyId, updateCompanyParams) {
+        if (updateCompanyParams.cnpj) {
+            const companyWithProvidedCnpj =
+                await this.getCompanyByCnpjRepository.execute(
+                    updateCompanyParams.cnpj,
+                )
+            if (
+                companyWithProvidedCnpj &&
+                companyWithProvidedCnpj.id !== companyId
+            ) {
+                throw new CnpjAlreadyInUseError(updateCompanyParams.cnpj)
+            }
+        }
+
+        if (updateCompanyParams.email) {
+            const companyWithProvidedEmail =
+                await this.getCompanyByEmailRepository.execute(
+                    updateCompanyParams.email,
+                )
+            if (
+                companyWithProvidedEmail &&
+                companyWithProvidedEmail.id !== companyId
+            ) {
+                throw new EmailAlreadyInUseError(updateCompanyParams.email)
+            }
+        }
+
+        const company = {
+            ...updateCompanyParams,
+        }
+
+        if (updateCompanyParams.password) {
+            const hashedPassword = this.passwordHasherAdapter.execute(
+                updateCompanyParams.password,
+            )
+
+            company.password = hashedPassword
+        }
+
+        const updateCompany = await this.updateCompanyRepository.execute(
+            companyId,
+            company,
+        )
+
+        return updateCompany
+    }
+}
