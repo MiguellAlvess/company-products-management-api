ce8f22efa919ec649ca56cd7cb6642599e4f8568
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-06 08:43:16 -0300

    feat: add PostgresUpdateCompanyRepository

---
+++ src/repositories/company/update-company.js
@@ -0,0 +1,26 @@
+import { prisma } from '../../../prisma/prisma.js'
+import { CompanyNotFoundError } from '../../errors/index.js'
+
+export class PostgresUpdateCompanyRepository {
+    async execute(companyId, updateCompanyParams) {
+        try {
+            return await prisma.company.update({
+                where: {
+                    companyId,
+                },
+                data: {
+                    ...updateCompanyParams,
+                },
+            })
+        } catch (error) {
+            if (error.name === 'PrismaClientKnownRequestError') {
+                // P2025 -> An operation failed because it depends on one or more records that were required but not found. {cause}
+                if (error.code === 'P2025') {
+                    throw new CompanyNotFoundError()
+                }
+            }
+
+            throw error
+        }
+    }
+}
